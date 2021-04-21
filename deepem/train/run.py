e362ae222967daa6dc7dbb03bfb1b4adca6a08dc
Kisuk Lee <kisuk@princeton.edu>
2021-04-21 13:37:26 +0900

    move backward out of autocast()

--- deepem/train/run.py
+++ deepem/train/run.py
@@ -45,9 +45,10 @@ def train(opt):
                 with torch.cuda.amp.autocast():
                     losses, nmasks, preds = forward(model, sample, opt)
                     total_loss = sum([w*losses[k] for k, w in opt.loss_weight.items()])
-                    scaler.scale(total_loss).backward()
-                    scaler.step(optimizer)
-                    scaler.update()
+                # Backward passes under autocast are not recommended.
+                scaler.scale(total_loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
                 losses = {k: v.float() for k, v in losses.items()}
                 nmasks = {k: v.float() for k, v in nmasks.items()}
                 preds  = {k: v.float() for k, v in preds.items()}
